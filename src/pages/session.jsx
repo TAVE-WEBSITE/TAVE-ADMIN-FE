b51e4e95b515b0206ec91a09c1c496168d3a5f47
tldms0507 <sieun050783@gmail.com>
2024-11-12 16:19:21 +0900

    feat : 드롭다운 컴포넌트 생성

--- src/pages/session.jsx
+++ src/pages/session.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Tab from "../components/tab";
 import Header from "../components/header";
 import SessionBlock from "../components/sessionBlock";
+import DropDown from "../components/dropdown";
+import useDropClose from "../hooks/dropClose";
+import ArrowDown from "../assets/images/arrowDown.png";
+import ArrowUp from "../assets/images/arrowUp.png";
 
 export default function Session() {
   const categories = ["정규세션", "동아리 이력", "후기", "문의"];
   const links = ["/regularsession", "/history", "/review", "/inquiry"];
 
+  const dropDownRef = useRef();
+
+  const [batch, setBatch] = useState("ALL");
+  const batchList = ["ALL", "14기", "13기", "12기"];
+
+  const [isOpen, setIsOpen] = useDropClose(dropDownRef, false);
   return (
     <div className="mt-40">
       <Header />
@@ -16,11 +26,19 @@ export default function Session() {
           <Tab category={categories} link={links} currentIndex={0} />
         </div>
         <div className="flex justify-between mt-16">
-          <p>드롭다운</p>
+          <div ref={dropDownRef} className="relative inline-block">
+            <DropDown
+              valueList={batchList}
+              setValue={setBatch}
+              isOpen={isOpen}
+              setIsOpen={setIsOpen}
+            />
+          </div>
+
           <p>세션추가</p>
         </div>
-              <div className="grid place-items-center grid-cols-3 gap-8 mt-12">
-                <SessionBlock/>
+        <div className="grid place-items-center grid-cols-3 gap-8 mt-12">
+          <SessionBlock />
         </div>
       </div>
     </div>
