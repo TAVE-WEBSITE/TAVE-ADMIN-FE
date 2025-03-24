ddd3541c5bcaf216c6e37fc4e051005d1473e644
PangKiYeon <pdrkeamy@gmail.com>
2025-03-25 01:33:09 +0900

    FIX : 설정 정리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-
 import './App.css';
 import './index.css';
+import './styles/global.css';
 
 function App() {
-    return (
-        <div className="App">
-           
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
